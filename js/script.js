3d9b27f15f8ed9cfc1386b6a029150ee6cd95248
Joachim Gonvers <joachim.gonvers@gmail.com>
2023-05-09 11:17:03 +0200

    added language change at load

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ function change_language(target) {
         elem.classList.remove("hide")
     }
 }
+
+function initial_language() {
+    const lang_list = navigator.languages
+    for (var lang of lang_list) {
+        if (lang.startsWith("en")) { lang = "en"; break; }
+        if (lang.startsWith("fr")) { lang = "fr"; break; }
+    }
+    switch (lang) {
+        case "en":
+            change_language(".english");
+            break;
+        case "fr":
+            change_language(".french");
+            break;
+        default:
+            change_language(".english");
+    }
+}
